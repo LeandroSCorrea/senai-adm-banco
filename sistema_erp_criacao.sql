f75c65a56c421dfd296def223436cf9bc25bf347
Leandro Corrêa <leandro@pop-os.tail4f6083.ts.net>
2025-12-17 00:12:48 -0300

    Término das criação das tabelas

--- sistema_erp_criacao.sql
+++ sistema_erp_criacao.sql
@@ -1,4 +1,6 @@
-CREATE DATABASE erp;
+-- CREATE DATABASE erp;
+
+use erp;
 
 CREATE TABLE empresa (
     id INT(10) PRIMARY KEY AUTO_INCREMENT,
@@ -9,10 +11,22 @@ CREATE TABLE empresa (
     inscricao_municipal VARCHAR(30) NOT NULL,
     matriz_filial CHAR(1) NOT NULL,
     data_cadastro DATE NOT NULL,
-    empresa_id INT(10) NOT NULL,
+    empresa_id INT(10),
 
     CONSTRAINT fk_empresa_id
-        FOREIGN KEY empresa_id(id) REFERENCES empresa(id)
+        FOREIGN KEY (empresa_id) REFERENCES empresa(id)
+);
+
+CREATE TABLE auditoria (
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    data_hora DATETIME NOT NULL,
+    tabela VARCHAR(100) NOT NULL,
+    antigo VARCHAR(100) NOT NULL,
+    novo VARCHAR(100) NOT NULL,
+    registro_id INT(11) NOT NULL,
+
+    CONSTRAINT fk_registro_id_auditoria
+        FOREIGN KEY (registro_id) REFERENCES empresa(id)
 );
 
 CREATE TABLE convenio (
@@ -86,7 +100,7 @@ CREATE TABLE colaborador (
         FOREIGN KEY nivel_formacao_id(id) REFERENCES nivel_formacao(id),
     CONSTRAINT fk_tipo_colaborador_id
         FOREIGN KEY tipo_colaborador_id(id) REFERENCES tipo_colaborador(id),
-    CONSTRAINT fk_cargo_id
+    CONSTRAINT fk_cargo_id_colaborador
         FOREIGN KEY cargo_id(id) REFERENCES cargo(id),
     CONSTRAINT fk_id_setor
         FOREIGN KEY id_setor(id) REFERENCES setor(id)
@@ -116,6 +130,29 @@ CREATE TABLE papel (
     descricao text
 );
 
+CREATE TABLE funcao (
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    descricao_menu VARCHAR(30) NOT NULL,
+    imagem_menu VARCHAR(30) NOT NULL,
+    metodo VARCHAR(30)
+);
+
+-- Tabela associativa entre papel e funçao
+CREATE TABLE papel_funcao (
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    pode_consultar CHAR(1),
+    pode_inserir CHAR(1),
+    pode_alterar CHAR(1),
+    pode_excluir CHAR(1),
+    funcao_id INT(10) NOT NULL,
+    papel_id INT(10) NOT NULL,
+
+    CONSTRAINT fk_funcao_id
+        FOREIGN KEY funcao_id(id) REFERENCES funcao(id),
+    CONSTRAINT fk_papel_id
+        FOREIGN KEY papel_id(id) REFERENCES papel(id)
+);
+
 CREATE TABLE usuario (
     id INT(10) PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(20) NOT NULL,
@@ -123,9 +160,9 @@ CREATE TABLE usuario (
     papel_id INT NOT NULL,
     colaborador_id INT NOT NULL,
 
-    CONSTRAINT fk_papel_id
+    CONSTRAINT fk_papel_id_usuario
         FOREIGN KEY papel_id(id) REFERENCES papel(id),
-    CONSTRAINT fk_colaborador_id
+    CONSTRAINT fk_colaborador_id_usuario
         FOREIGN KEY colaborador_id(id) REFERENCES colaborador(id)
 );
 
@@ -141,7 +178,7 @@ CREATE TABLE cliente (
     cpf_cnpj VARCHAR(14) NOT NULL,
     rg VARCHAR(20) NOT NULL,
     orgao_rg VARCHAR(10) NOT NULL,
-    inscricao_estadual VARCHAR(30) NOT NULL, -- Precisa? Ou ela virá da tabela empresa?
+    inscricao_estadual VARCHAR(30) NOT NULL,
     inscricao_municipal VARCHAR(30) NOT NULL,
     desde DATE not null,
     tipo_pessoa CHAR(1) NOT NULL ,
@@ -156,6 +193,14 @@ CREATE TABLE cliente (
         FOREIGN KEY id_empresa(id) REFERENCES empresa(id)
 );
 
+-- Tabela associada a produto e fornecedor para a correta aplicação e cálculo de impostos
+CREATE TABLE cfop (
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    cfop INT(10) NOT NULL,
+    descricao TEXT,
+    aplicacao TEXT
+);
+
 CREATE TABLE tipo_fornecedor (
     id INT(10) PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(20) NOT NULL,
@@ -168,7 +213,7 @@ CREATE TABLE fornecedor (
     cpf_cnpj VARCHAR(14) NOT NULL,
     rg VARCHAR(20) NOT NULL,
     orgao_rg VARCHAR(10) NOT NULL,
-    inscricao_estadual VARCHAR(30) NOT NULL, -- Precisa? Ou ela virá da tabela empresa?
+    inscricao_estadual VARCHAR(30) NOT NULL,
     inscricao_municipal VARCHAR(30) NOT NULL,
     desde DATE not null,
     tipo_pessoa CHAR(1) NOT NULL ,
@@ -177,13 +222,16 @@ CREATE TABLE fornecedor (
     situacao_for_cli_id INT(10) NOT NULL,
     id_empresa INT(10) NOT NULL,
     tipo_fornecedor_id INT(10) NOT NULL,
+    cfop_id INT(10) NOT NULL,
 
     CONSTRAINT fk_situacao_for_cli_id_fornecedor
         FOREIGN KEY situacao_for_cli_id(id) REFERENCES situacao_for_cli(id),
     CONSTRAINT fk_id_empresa_fornecedor
         FOREIGN KEY id_empresa(id) REFERENCES empresa(id),
     CONSTRAINT fk_tipo_fornecedor_id
-        FOREIGN KEY tipo_fornecedor_id(id) REFERENCES tipo_fornecedor(id)
+        FOREIGN KEY tipo_fornecedor_id(id) REFERENCES tipo_fornecedor(id),
+    CONSTRAINT fk_cfop_id
+        FOREIGN KEY cfop_id(id) REFERENCES cfop(id)
 );
 
 CREATE TABLE cidade (
@@ -193,10 +241,72 @@ CREATE TABLE cidade (
     nome2 VARCHAR(100)
 );
 
-CREATE TABLE banco (
+CREATE TABLE pais (
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    codigo INT(10) NOT NULL,
+    nome_en VARCHAR(100) NOT NULL,
+    nome_ptbr VARCHAR(100) NOT NULL,
+    sigla2 CHAR(2) NOT NULL,
+    sigla3 CHAR(3) NOT NULL
+);
+
+CREATE TABLE indice_economico (
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    sigla VARCHAR(50) NOT NULL,
+    nome VARCHAR(50) NOT NULL,
+    descricao TEXT,
+    pais_id INT(10) NOT NULL,
+
+    CONSTRAINT fk_pais_id
+        FOREIGN KEY pais_id(id) REFERENCES pais(id)
+);
+
+CREATE TABLE estado (
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    codigo_ibge INT(10) NOT NULL,
+    pais_id INT(10) NOT NULL,
+    sigla CHAR(2) NOT NULL,
+
+    CONSTRAINT fk_pais_id_estado
+        FOREIGN KEY pais_id(id) REFERENCES pais(id)
+);
+
+CREATE TABLE log_cidade (
     id INT(10) PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
-    url VARCHAR(255) NOT NULL
+    codigo_ibge INT(11) NOT NULL,
+    data_alteracao TIMESTAMP,
+    operacao INT(11) NOT NULL,
+    id_cidade INT(11) NOT NULL,
+    estado_id INT(11) NOT NULL,
+
+    CONSTRAINT fk_id_cidade
+        FOREIGN KEY id_cidade(id) REFERENCES cidade(id),
+    CONSTRAINT fk_estado_id
+        FOREIGN KEY estado_id(id) REFERENCES estado(id)
+);
+
+CREATE TABLE operadora_cartao (
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    bandeira VARCHAR(30)
+);
+
+CREATE TABLE url (
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    site VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE banco (
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    codigo INT(10) NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    url_id INT(11) NOT NULL,
+
+    CONSTRAINT fk_codigo_banco
+        FOREIGN KEY (url_id) REFERENCES url(id)
 );
 
 CREATE TABLE cep (
@@ -211,7 +321,6 @@ CREATE TABLE cep (
         FOREIGN KEY cidade_id(id) REFERENCES cidade(id)
 );
 
--- Tabela associativa entre cep e banco
 CREATE TABLE agencia_banco (
     id INT(10) PRIMARY KEY AUTO_INCREMENT,
     codigo INT(10) NOT NULL,
@@ -229,7 +338,6 @@ CREATE TABLE agencia_banco (
     CONSTRAINT fk_banco_id
         FOREIGN KEY banco_id(id) REFERENCES banco(id)
 );
-
 
 CREATE TABLE tipo_endereco (
     id INT(10) PRIMARY KEY AUTO_INCREMENT,
@@ -253,7 +361,7 @@ CREATE TABLE endereco (
 
     CONSTRAINT fk_empresa_id_endereco
         FOREIGN KEY empresa_id(id) references empresa(id),
-    CONSTRAINT fk_colaborador_id
+    CONSTRAINT fk_colaborador_id_endereco
         FOREIGN KEY colaborador_id(id) references colaborador(id),
     CONSTRAINT fk_fornecedor_id_endereco
         FOREIGN KEY fornecedor_id(id) REFERENCES fornecedor(id),
@@ -261,6 +369,135 @@ CREATE TABLE endereco (
         FOREIGN KEY cliente_id(id) REFERENCES cliente(id),
     CONSTRAINT fk_tipo_endereco_id
         FOREIGN KEY tipo_endereco_id(id) REFERENCES tipo_endereco(id),
-    CONSTRAINT fk_cep_id
+    CONSTRAINT fk_cep_id_endereco
         FOREIGN KEY cep_id(id) REFERENCES cep(id)
+);
+
+CREATE TABLE contato (
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    dono CHAR(1) NOT NULL,
+    empresa_id INT(10) NOT NULL,
+    colaborador_id INT(10) NOT NULL,
+    cliente_id INT(10) NOT NULL,
+    fornecedor_id INT(10) NOT NULL,
+    contato_id INT(10) NOT NULL,
+
+    CONSTRAINT fk_empresa_id_contato
+        FOREIGN KEY empresa_id(id) REFERENCES empresa(id),
+    CONSTRAINT fk_colaborador_id_contato
+        FOREIGN KEY colaborador_id(id) REFERENCES colaborador(id),
+    CONSTRAINT fk_cliente_id_contato
+        FOREIGN KEY empresa_id(id) REFERENCES empresa(id),
+    CONSTRAINT fk_fornecedor_id
+        FOREIGN KEY fornecedor_id(id) REFERENCES fornecedor(id)
+);
+
+CREATE TABLE tipo_email (
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(20) NOT NULL,
+    descricao TEXT
+);
+
+CREATE TABLE log_tipo_email (
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    operacao VARCHAR(20) NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    descricao VARCHAR(200) NOT NULL,
+    data_exclusao TIMESTAMP NOT NULL,
+    id_tabela INT(11) NOT NULL,
+
+    CONSTRAINT fk_id_tabela
+        FOREIGN KEY id_tabela(id) REFERENCES tipo_email(id)
+);
+
+-- Tabela associativa entre contato e tipo_email
+CREATE TABLE contato_email (
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(100) NOT NULL,
+    tipo_email_id INT(10) NOT NULL,
+    contato_id INT(10) NOT NULL,
+
+    CONSTRAINT fk_tipo_email_id
+        FOREIGN KEY tipo_email_id(id) REFERENCES tipo_email(id),
+    CONSTRAINT fk_contato_id
+        FOREIGN KEY contato_id(id) REFERENCES contato(id)
+);
+
+CREATE TABLE tipo_telefone (
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(20) NOT NULL,
+    descricao TEXT
+);
+
+-- Tabela associativa entre contato e tipo_telefone
+CREATE TABLE contato_telefone (
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    telefone VARCHAR(10) NOT NULL,
+    tipo_telefone_id INT(10) NOT NULL,
+    contato_id INT(10) NOT NULL,
+
+    CONSTRAINT fk_tipo_telefone_id_contato_telefone
+        FOREIGN KEY tipo_telefone_id(id) REFERENCES tipo_telefone(id),
+    CONSTRAINT fk_contato_id_contato_telefone
+        FOREIGN KEY contato_id(id) REFERENCES contato(id)
+);
+
+CREATE TABLE grupo_produto(
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    nome  VARCHAR(20) NOT NULL,
+    descricao TEXT
+);
+
+CREATE TABLE sub_grupo_produto (
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(20) NOT NULL,
+    descricao TEXT,
+    id_grupo INT(10) NOT NULL,
+
+    CONSTRAINT fk_id_grupo
+        FOREIGN KEY id_grupo(id) REFERENCES grupo_produto(id)
+);
+
+CREATE TABLE unidade_produto (
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(10) NOT NULL,
+    descricao TEXT NOT NULL
+);
+
+CREATE TABLE produto (
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    nome_produto VARCHAR(30) NOT NULL,
+    descricao TEXT NOT NULL,
+    descricao_pdv VARCHAR(30) NOT NULL ,
+    valor_compra DOUBLE(11,2) NOT NULL,
+    valor_venda DOUBLE(11,2) NOT NULL,
+    qtd_estoque DOUBLE(11,2) NOT NULL,
+    estoque_min DOUBLE(11,2) NOT NULL,
+    estoque_max DOUBLE(11,2) NOT NULL,
+    excluido CHAR(1) NOT NULL,
+    data_cadastro DATE NOT NULL,
+    id_sub_grupo INT(10) NOT NULL,
+    id_unidade INT(10) NOT NULL,
+    gtin CHAR(14) NOT NULL,
+    cfop_id INT(10) NOT NULL,
+
+    CONSTRAINT fk_id_sub_grupo_produto
+        FOREIGN KEY (id_sub_grupo) REFERENCES sub_grupo_produto(id),
+    CONSTRAINT fk_id_unidade_produto
+        FOREIGN KEY (id_unidade) REFERENCES unidade_produto(id),
+    CONSTRAINT fk_cfop_id_produto
+        FOREIGN KEY (cfop_id) REFERENCES cfop(id)
+);
+
+-- Tabela associativa entre empre e produto
+CREATE TABLE empresa_produto (
+    id INT(10) PRIMARY KEY AUTO_INCREMENT,
+    empresa_id INT(10) NOT NULL,
+    produto_id INT(10) NOT NULL,
+
+    CONSTRAINT fk_empresa_id_empresa_produto
+        FOREIGN KEY (empresa_id) REFERENCES empresa(id),
+    CONSTRAINT fk_produto_id_empresa_produto
+        FOREIGN KEY (produto_id) REFERENCES produto(id)
 );
